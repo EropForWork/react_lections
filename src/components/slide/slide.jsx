ad7f07525bb50265c2450ef859e02180f84cc00b
Erop <u4iha1@gmail.com>
2024-05-13 18:44:48 +0300

    init

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -3,14 +3,12 @@ import './slide.css';
 import { useSlideContext } from '../slideContext/slideContext';
 import SlideTitle from '../slideTitle/slideTitle';
 import SlideSubtitle from '../subtitle/subtitle';
-import Column from '../column/column';
-import TextContent from '../textContent/textContent';
 import ColumnContainer from '../columnContainer/columnContainer';
 
 function Slide({ slideContext }) {
 	const { value: slideValue } = useSlideContext();
 	if (!slideContext && slideValue.length > 0) {
-		const { title, subtitle, columns, textContent } = slideValue[0]
+		const { title, subtitle, columns } = slideValue[0]
 		return (
 			<div className="slide">
 				{
@@ -21,9 +19,6 @@ function Slide({ slideContext }) {
 				}
 				{
 					columns && <ColumnContainer value={columns} />
-				}
-				{
-					// textContent && <TextContent value={textContent} />
 				}
 			</div>
 		);
--- src/components/slideTitle/slideTitle.jsx
+++ src/components/slideTitle/slideTitle.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import './slideTitle.css';
 
 export default function SlideTitle({ value }) {
-
+    const handleClick = (e) => {
+        console.log(e.target);
+    };
     return (
-        <div className="slide-title-container">
+        <div className="slide-title-container" onClick={handleClick}>
             <h1 className="slide-title">{value}</h1>
         </div>
     )
